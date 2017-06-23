596e889829c017a943892fe686e893b813ed7e44
user <user@MA324-37>
2017-06-23 15:55:59 +0800

    finish

---
+++ ASP.NET-FinalTermExam/Controllers/CustomersController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_FinalTermExam.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<SelectListItem> returnContactTitleSelect()
+        {
+            List<SelectListItem> contactTitleSelect = new List<SelectListItem>();
+            List<Models.Cust> contactTitleData = Models.FinalCustomers.GetContactTitle();
+            contactTitleSelect.Add(new SelectListItem()
+            {
+                Text = "",
+                Value = "",
+                Selected = true
+            });
+            foreach (var item in contactTitleData)
+            {
+                contactTitleSelect.Add(new SelectListItem()
+                {
+                    Text = item.ContactTitle,
+                    Value = item.ContactTitle
+                });
+            }
+            return contactTitleSelect;
+        }
+        // GET: Customers
+        [HttpGet()]
+        public ActionResult Index()
+        {
+            Models.Cust cust = new Models.Cust();
+
+            ViewBag.contactTitleSelect = returnContactTitleSelect();
+
+            Models.FinalCustomers FinalCustomers = new Models.FinalCustomers();
+            List<Models.Cust> searchCust = FinalCustomers.SearchCustomers(cust);
+            ViewBag.searchResult = searchCust;
+            return View();
+        }
+        [HttpPost()]
+        public ActionResult Index(Models.Cust cust)
+        {
+            ViewBag.contactTitleSelect = returnContactTitleSelect();
+
+            Models.FinalCustomers FinalCustomers = new Models.FinalCustomers();
+            List<Models.Cust> searchCust = FinalCustomers.SearchCustomers(cust);
+            ViewBag.searchResult = searchCust;
+            return View();
+        }
+
+        public ActionResult NewCustomers()
+        {
+            ViewBag.contactTitleSelect = returnContactTitleSelect();
+            return View();
+        }
+
+        public ActionResult NewCustomersFinish(Models.Cust cust)
+        {
+            Models.FinalCustomers.NewCustomers(cust);
+            return View("Index");
+        }
+    }
+    
+}
---
+++ ASP.NET-FinalTermExam/Models/FinalCustomers.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASP.NET_FinalTermExam.Models
+{
+    public class FinalCustomers
+    {
+        private static string GetDBConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["DBconnect"].ConnectionString.ToString();
+        }
+
+        public static List<Cust> GetContactTitle()
+        {
+            List<Cust> result = new List<Cust>();
+            DataTable dt = new DataTable();
+            string sql = @" SELECT CodeId + '-' + CodeVal AS ContactTitle
+                            FROM dbo.CodeTable
+                            WHERE CodeType LIKE 'TITLE'";
+            using (SqlConnection conn = new SqlConnection(GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(new Cust()
+                {
+                    ContactTitle = row["ContactTitle"].ToString()
+                });
+            }
+            return result;
+        }
+
+        public List<Cust> SearchCustomers(Cust cust)
+        {
+            List<Cust> result = new List<Cust>();
+            DataTable dt = new DataTable();
+            string sql = @" SELECT CustomerID, CompanyName, ContactName, ContactTitle + '-' + CodeVal AS ContactTitle
+                            FROM dbo.CodeTable AS code JOIN Sales.Customers AS cust ON code.CodeId = cust.ContactTitle
+                            WHERE code.CodeType LIKE 'TITLE' AND ";
+            if (cust.CustomerID != 0)
+            {
+                sql += "CustomerID = " + cust.CustomerID + " AND ";
+            }
+            if (cust.CompanyName != null)
+            {
+                sql += "CompanyName LIKE '%" + cust.CompanyName + "%' AND ";
+            }
+            if (cust.ContactName != null)
+            {
+                sql += "ContactName LIKE '%" + cust.ContactName + "%' AND ";
+            }
+            if (cust.ContactTitle != null)
+            {
+                sql += "ContactTitle + '-' + CodeVal LIKE '%" + cust.ContactTitle + "%' AND ";
+            }
+            sql += "1=1";
+            using (SqlConnection conn = new SqlConnection(GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(new Cust()
+                {
+                    CustomerID = (int)row["CustomerID"],
+                    CompanyName = row["CompanyName"].ToString(),
+                    ContactName = row["ContactName"].ToString(),
+                    ContactTitle = row["ContactTitle"].ToString()
+                });
+            }
+            return result;
+
+        }
+
+        public static void NewCustomers(Cust cust)
+        {
+            string sql = @" INSERT INTO Sales.Customers(CustomerID, CompanyName, ContactName, ContactTitle, CreationDate, Country, City, Region, PostalCode, Address, Phone, Fax)
+                            VALUES(NULL, @CompanyName, @ContactName, @ContactTitle, @CreationDate, @Country, @City, @Region, @PostalCode, @Address, @Phone, @Fax)";
+            using (SqlConnection conn = new SqlConnection(GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.Add(new SqlParameter("@CompanyName", cust.CompanyName == null ? string.Empty : cust.CompanyName));
+                cmd.Parameters.Add(new SqlParameter("@ContactName", cust.ContactName == null ? string.Empty : cust.ContactName));
+                cmd.Parameters.Add(new SqlParameter("@ContactTitle", cust.ContactTitle.Substring(0, 4)));
+                cmd.Parameters.Add(new SqlParameter("@CreationDate", cust.CreationDate));
+                cmd.Parameters.Add(new SqlParameter("@Country", cust.Country == null ? string.Empty : cust.Country));
+                cmd.Parameters.Add(new SqlParameter("@City", cust.City == null ? string.Empty : cust.City));
+                cmd.Parameters.Add(new SqlParameter("@Region", cust.Region == null ? string.Empty : cust.Region));
+                cmd.Parameters.Add(new SqlParameter("@PostalCode", cust.PostalCode == null ? string.Empty : cust.PostalCode));
+                cmd.Parameters.Add(new SqlParameter("@Address", cust.Address == null ? string.Empty : cust.Address));
+                cmd.Parameters.Add(new SqlParameter("@Phone", cust.Phone == null ? string.Empty : cust.Phone));
+                cmd.Parameters.Add(new SqlParameter("@Fax", cust.Fax == null ? string.Empty : cust.Fax));
+
+                cmd.ExecuteScalar();
+                conn.Close();
+            }
+        }
+    }
+}
